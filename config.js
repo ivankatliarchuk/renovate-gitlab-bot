4833cc4afd1e34608198cd4ed9a8fc5f935c3ccc
Lukas Eipert <leipert@gitlab.com>
2019-01-28 01:26:11 +0100

    Change strategy to lockfile-update

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const updateNothing = {
 const updateGitLabScope = {
   packagePatterns: ["@gitlab/.*"],
   enabled: true,
-  rangeStrategy: "bump"
+  rangeStrategy: "lockfile-update"
 };
 
 const prBodyNotes = [
@@ -56,7 +56,7 @@ module.exports = {
       repository,
       ...baseConfig,
       prBodyNotes,
-      rangeStrategy: "bump"
+      rangeStrategy: "lockfile-update"
     }))
   ]
 };
