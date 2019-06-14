7387b1c277d58158c600e7bed2057ee63d444730
Lukas 'Eipi' Eipert <leipert@gitlab.com>
2019-06-14 21:34:23 +0000

    Fix installation

--- run-renovate.sh
+++ run-renovate.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 export RENOVATE_CONFIG_FILE="./config.js"
+export RENOVATE_TOKEN=ksk3hZZLMwDdS1NHxe99
 export RENOVATE_DISABLE_FILE_RECURSION=true
 
 yarn install --ignore-optional
+# PATH=$PATH:$PWD/node_modules/.bin
 cd node_modules/renovate
+yarn install --ignore-optional
 yarn run build
 cd ../..
 node node_modules/renovate/dist/renovate.js
