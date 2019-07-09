6b65e5eb32bafa5c262784eb1e36c2e2a5c2cfcb
Lukas Eipert <leipert@gitlab.com>
2019-07-09 16:36:30 +0200

    Increase heap memory size

--- run-renovate.sh
+++ run-renovate.sh
@@ -2,4 +2,5 @@
 export RENOVATE_CONFIG_FILE="./config.js"
 export RENOVATE_DISABLE_FILE_RECURSION=true
 
+export NODE_OPTIONS="--max-old-space-size=4096"
 node ./node_modules/renovate/dist/renovate.js
