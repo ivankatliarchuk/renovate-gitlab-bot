879cf7a6160f7803f314d94824e237b3ce4c0508
Lukas Eipert <leipert@gitlab.com>
2021-03-24 12:10:14 +0100

    Build renovate from fork

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+if ! [ -d renovate-fork ]; then
+  git clone https://gitlab.com/gitlab-org/frontend/renovate-fork.git
+fi
+
+cd renovate-fork || exit 1
+git checkout gitlab-main
+git pull
+yarn install
+yarn build
+yarn pack
+mv ./renovate*.tgz ../renovate-fork.tgz
+cd .. || exit 1
+
+yarn install --production --frozen-lockfile --force
+
+if [ "$DOCKER_BUILD" = "true" ]; then
+  echo "Cleaning up the renovate-fork source files"
+  rm -rf renovate-fork renovate-fork.tgz
+fi
