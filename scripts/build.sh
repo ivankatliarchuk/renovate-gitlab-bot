b7ac96b9a7271aa9025857b62a6ca8d950ad6041
Lukas Eipert <leipert@gitlab.com>
2022-06-10 13:55:58 +0200

    Update to renovate@29

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env sh
 
+FORK_BRANCH=gitlab-main-v29
+
 if ! [ -d renovate-fork ]; then
   git clone https://gitlab.com/gitlab-org/frontend/renovate-fork.git
 fi
 
+rm -f renovate*.tgz
+
 cd renovate-fork || exit 1
 git fetch
-git checkout gitlab-main-v28
+git checkout "$FORK_BRANCH"
 git pull
 yarn install
 yarn build
 yarn pack
-mv ./renovate*.tgz ../renovate-fork.tgz
+mv ./renovate*.tgz "../renovate-fork-$FORK_BRANCH.tgz"
 cd .. || exit 1
 
-yarn install --production --frozen-lockfile --force
-
 if [ "$DOCKER_BUILD" = "true" ]; then
+  yarn install --production --frozen-lockfile --force
   echo "Cleaning up the renovate-fork source files"
-  rm -rf renovate-fork renovate-fork.tgz
+  rm -rf renovate-fork renovate*.tgz
+else
+  yarn add "renovate@file:./renovate-fork-$FORK_BRANCH.tgz"
+  yarn install --force
 fi
