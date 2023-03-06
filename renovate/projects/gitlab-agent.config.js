9e808e69dafbe229538e94239830ac25601ba835
Lukas Eipert <leipert@gitlab.com>
2023-03-06 13:38:57 +0100

    Fix regex

--- renovate/projects/gitlab-agent.config.js
+++ renovate/projects/gitlab-agent.config.js
@@ -18,7 +18,7 @@ module.exports = createServerConfig([
       {
         fileMatch: [".gitlab/.gitlab-ci.yml"],
         matchStrings: [
-          "\n\\s*BUILD_IMAGE_SHA:\\s*\"(?<currentValue>[^@]+)@(?<currentDigest>sha256:[a-f0-9]+)\"\n",
+          '\n\\s*BUILD_IMAGE_SHA:\\s*"(?<currentValue>[^@]+)@(?<currentDigest>sha256:[a-f0-9]+)"\n',
         ],
         depNameTemplate: "gitlab-agent-ci-image",
         packageNameTemplate: "gitlab/gitlab-agent-ci-image",
@@ -27,7 +27,7 @@ module.exports = createServerConfig([
       {
         fileMatch: [".gitlab/.gitlab-ci.yml"],
         matchStrings: [
-          "\n\\s*FIPS_BUILD_IMAGE_SHA:\\s*\"(?<currentValue>[^@]+)@(?<currentDigest>sha256:[a-f0-9]+)\"\n",
+          '\n\\s*FIPS_BUILD_IMAGE_SHA:\\s*"(?<currentValue>[^@]+)@(?<currentDigest>sha256:[a-f0-9]+)"\n',
         ],
         depNameTemplate: "gitlab-agent-ci-image",
         packageNameTemplate: "gitlab/gitlab-agent-ci-image",
@@ -35,9 +35,7 @@ module.exports = createServerConfig([
       },
       {
         fileMatch: [".gitlab/.gitlab-ci.yml"],
-        matchStrings: [
-          "\n\\s*DOCKER_VERSION:\\s*\"(?<currentValue>[^\"]+)\"\n",
-        ],
+        matchStrings: ['\n\\s*DOCKER_VERSION:\\s*"(?<currentValue>[^"]+)"\n'],
         depNameTemplate: "docker",
         datasourceTemplate: "docker",
       },
@@ -45,16 +43,16 @@ module.exports = createServerConfig([
         fileMatch: ["WORKSPACE"],
         matchStrings: [
           "\n#\\s*(?<currentValue>\\S+)\\s+from.*?\n" +
-          "\\s*container_pull(\n" +
-          "\\s*name\\s*=\\s*\"[^\"]\",\n" +
-          "\\s*digest\\s*=\\s*\"(?<currentDigest>sha256:[a-f0-9]+)\",\n" +
-          "\\s*registry\\s*=\\s*\"(?<registry>[^\"]+)\",\n" +
-          "\\s*repository\\s*=\\s*\"(?<repository>[^\"]+)\",\n" +
-          "\\s*)",
+            "\\s*container_pull\\(\n" +
+            '\\s*name\\s*=\\s*"[^"]+",\n' +
+            '\\s*digest\\s*=\\s*"(?<currentDigest>sha256:[a-f0-9]+)",\n' +
+            '\\s*registry\\s*=\\s*"(?<registry>[^"]+)",\n' +
+            '\\s*repository\\s*=\\s*"(?<repository>[^"]+)",\n' +
+            "\\s*\\)",
         ],
         depNameTemplate: "{{{registry}}}/{{{repository}}}",
         datasourceTemplate: "docker",
-      }
-    ]
-  }
+      },
+    ],
+  },
 ]);
