f7e01651d2c84b2851fa649ab2e269f044aec5c5
Birger J. Nordølum <contact@mindtooth.no>
2023-03-23 12:02:12 +0000

    feat: add gitlab-org/terraform-images config

---
+++ renovate/projects/terraform-images.config.js
@@ -0,0 +1,60 @@
+const {
+  createServerConfig,
+  baseConfig,
+  defaultLabels,
+} = require("../shared");
+
+module.exports = createServerConfig([
+  {
+    repository: "gitlab-renovate-forks/terraform-images",
+    ...baseConfig,
+    labels: [
+      ...defaultLabels,
+      "group::configure",
+      "devops::configure",
+      "section::ops",
+      "Category:Infrastructure as Code",
+    ],
+    enabledManagers: ["regex"],
+    packageRules: [
+      {
+        matchPackageNames: "hashicorp/terraform",
+        matchManagers: ["regex"],
+        separateMinorPatch: true,
+      },
+      {
+        matchPackageNames: "hashicorp/terraform",
+        matchManagers: ["regex"],
+        matchUpdateTypes: ["major", "minor"],
+        enabled: false,
+      },
+    ],
+    regexManagers: [
+      {
+        fileMatch: ["^.gitlab-ci.yml$"],
+        matchStrings: ['BASE_IMAGE: "(?<depName>.*):(?<currentValue>.*)"\\s'],
+        datasourceTemplate: "docker",
+      },
+      {
+        fileMatch: ["^.gitlab-ci.yml$"],
+        matchStrings: ['BUILDX_VERSION: "(?<currentValue>.*)"\\s'],
+        depNameTemplate: "docker/buildx",
+        datasourceTemplate: "github-releases",
+      },
+      {
+        fileMatch: ["^.gitlab-ci.yml$"],
+        matchStrings: [
+          'DOCKER_DIND_IMAGE: "(?<depName>.*):(?<currentValue>.*)"\\s',
+        ],
+        datasourceTemplate: "docker",
+      },
+      {
+        fileMatch: ["^.gitlab-ci.yml$"],
+        matchStrings: ['TERRAFORM_BINARY_VERSION: "(?<currentValue>.*)"\\s'],
+        depNameTemplate: "hashicorp/terraform",
+        datasourceTemplate: "github-releases",
+        extractVersionTemplate: "^v(?<version>.*)$",
+      },
+    ],
+  },
+]);
