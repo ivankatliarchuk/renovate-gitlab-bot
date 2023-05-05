4c7c804db2ce4246e429fb1ba0fa5e8fb4505cc1
Ash McKenzie <amckenzie@gitlab.com>
2023-05-05 11:53:30 +1000

    Add renovate support for gitlab-shell

---
+++ renovate/gitlab-shell/gitlab-shell.config.js
@@ -0,0 +1,27 @@
+const { createServerConfig, baseConfig, defaultLabels } = require("../shared");
+
+module.exports = createServerConfig([
+  {
+    repository: "gitlab-renovate-forks/gitlab-shell",
+    ...baseConfig,
+    labels: [
+      ...defaultLabels,
+    ],
+    reviewers: [
+      "ashmckenzie",
+    ],
+    reviewersSampleSize: 1,
+    enabledManagers: ["asdf", "bundler", "gomod"],
+    prConcurrentLimit: 4,
+    semanticCommits: "disabled",
+    packageRules: [
+      {
+        schedule: ["before 05:00 on Monday"],
+        matchPackagePatterns: [".+"],
+        rangeStrategy: "bump",
+        matchManagers: ["bundler"],
+        groupName: "Ruby dependencies",
+      },
+    ],
+  },
+]);
