9d4f571023edfdf9115fde6327965d5fe83d845e
Evan Read <eread@gitlab.com>
2023-10-09 14:18:10 +1000

    Stop automatically updating glob dependency

--- renovate/gitlab-docs/gitlab-docs.config.js
+++ renovate/gitlab-docs/gitlab-docs.config.js
@@ -18,7 +18,7 @@ module.exports = createServerConfig([
     packageRules: [
       ...updateNodeJS.packageRules,
       {
-        matchPackagePatterns: ["bootstrap", "vue"],
+        matchPackagePatterns: ["bootstrap", "glob", "vue"],
         enabled: false,
       },
       {
