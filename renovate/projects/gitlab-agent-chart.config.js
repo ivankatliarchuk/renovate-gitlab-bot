49b1a794c3a8a7f01da25a5d8f5a36b5ee419cd1
Taka Nishida <tnishida@gitlab.com>
2024-02-22 14:59:43 +0900

    Set up renovate for helm chart for GitLab Agents

---
+++ renovate/projects/gitlab-agent-chart.config.js
@@ -0,0 +1,34 @@
+const {
+  createServerConfig,
+  baseConfig,
+  defaultLabels,
+} = require("../lib/shared");
+
+const groupEnvironmentsLabels = [
+  ...defaultLabels,
+  "group::environments",
+  "devops::deploy",
+  "section::cd",
+];
+
+module.exports = createServerConfig([
+  {
+    repository: "gitlab-renovate-forks/charts-gitlab-agent",
+    ...baseConfig,
+    enabledManagers: ["regex"],
+    semanticCommits: "disabled",
+    reviewers: ["timofurrer", "ash2k", "takax"],
+    reviewersSampleSize: 2,
+    labels: groupEnvironmentsLabels,
+    regexManagers: [
+      {
+        fileMatch: ["Chart.yaml"],
+        matchStrings: ["appVersion: (?<currentValue>.*)\n"],
+        datasourceTemplate: "gitlab-releases",
+        registryUrlTemplate: "https://gitlab.com",
+        depNameTemplate: "gitlab-agent",
+        packageNameTemplate: "gitlab-org/cluster-integration/gitlab-agent",
+      },
+    ],
+  },
+]);
