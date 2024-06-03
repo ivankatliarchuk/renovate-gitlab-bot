70a780483f220dffd258a679a4cfdd4643785a7c
Jay McCure <jmccure@gitlab.com>
2024-06-03 12:29:30 +0000

    fix(gitlab cli): Updating golang across the board

--- renovate/projects/cli.config.js
+++ renovate/projects/cli.config.js
@@ -1,53 +1,54 @@
 const { updateNodeJS } = require("../lib/languages");
 const {
-    createServerConfig,
-    baseConfig,
-    defaultLabels,
-    availableRouletteReviewerByRole,
+  createServerConfig,
+  baseConfig,
+  defaultLabels,
+  availableRouletteReviewerByRole,
 } = require("../lib/shared");
-  
-module.exports = createServerConfig(
-    [
-        {
-            repository: "gitlab-renovate-forks/cli",
-            ...baseConfig,
-            reviewers: availableRouletteReviewerByRole("gitlab-cli"),
-            reviewersSampleSize: 1,
-            labels: [
-            ...defaultLabels,
-            "group::code review",
-            "devops::create",
-            "section::dev",
-            "Category:GitLab CLI",
-            "type::maintenance",
-            ],
-            rangeStrategy: "bump",
-            includePaths: ["*", "scripts/commit-lint/*"],
-            enabledManagers: ["asdf", "custom.regex", "gomod", "npm"],
-            postUpdateOptions: ["gomodTidy", "gomodUpdateImportPaths"],
-            packageRules: [
-                {
-                    // We want updates to golang, but not vale in asdf
-                    matchManagers: ["asdf"],
-                    matchPackageNames: ["vale"],
-                    enabled: false,
-                },
-                {
-                    matchManagers: ["asdf", "gomod", "custom.regex"],
-                    matchPackageNames: ["golang"],
-                },
-            ],
-            customManagers: [
-                {
-                    customType: "regex",
-                    fielMatch: ["^.gitlab-ci.yml"],
-                    matchStrings: ['GO_VERSION: "?(?<currentValue>.*)"?\n'],
-                    depNameTemplate: "gomod",
-                    datasourceTemplate: "gomod",
-                    versioningTemplate: "go-mod-directive",
-                },
-            ],
-            dependencyDashboard: false,
-        }
-    ]
-);
+
+module.exports = createServerConfig([
+  {
+    repository: "gitlab-renovate-forks/cli",
+    ...baseConfig,
+    reviewers: availableRouletteReviewerByRole("gitlab-cli"),
+    reviewersSampleSize: 1,
+    labels: [
+      ...defaultLabels,
+      "group::code review",
+      "devops::create",
+      "section::dev",
+      "Category:GitLab CLI",
+      "type::maintenance",
+    ],
+    rangeStrategy: "bump",
+    includePaths: ["*", "scripts/commit-lint/*"],
+    enabledManagers: ["asdf", "custom.regex", "gomod", "npm"],
+    postUpdateOptions: ["gomodTidy", "gomodUpdateImportPaths"],
+    packageRules: [
+      {
+        // We want updates to golang, but not vale in asdf
+        matchManagers: ["asdf"],
+        matchPackageNames: ["vale"],
+        enabled: false,
+      },
+      {
+        // Group golang updates across asdf, gomod and GitLab CI
+        matchManagers: ["asdf", "gomod", "custom.regex"],
+        matchPackageNames: ["golang", "go"],
+        groupName: "golang",
+      },
+    ],
+    customManagers: [
+      {
+        customType: "regex",
+        fileMatch: ["^.gitlab-ci.yml"],
+        matchStrings: ["GO_VERSION: [\"'](?<currentValue>.*?)[\"']"],
+        depNameTemplate: "go",
+        depTypeTemplate: "golang",
+        datasourceTemplate: "golang-version",
+        versioningTemplate: "go-mod-directive",
+      },
+    ],
+    dependencyDashboard: false,
+  },
+]);
