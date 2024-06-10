465b6ae1ab6e4cc960b51df9516eef1be91966db
Timo Furrer <tfurrer@gitlab.com>
2024-06-10 11:59:20 +0000

    feat: upgrade OpenTofu versions

--- renovate/projects/opentofu-component.config.js
+++ renovate/projects/opentofu-component.config.js
@@ -5,41 +5,57 @@ const {
   availableRouletteReviewerByRole,
 } = require("../lib/shared");
 
-module.exports = createServerConfig([
+module.exports = createServerConfig(
+  [
+    {
+      repository: "gitlab-renovate-forks/opentofu",
+      ...baseConfig,
+      extends: ["group:commitlintMonorepo"],
+      labels: [
+        ...defaultLabels,
+        "group::environments",
+        "Category:Infrastructure as Code",
+      ],
+      enabledManagers: ["gitlabci", "custom.regex"],
+      reviewers: ["timofurrer"],
+      packageRules: [
+        {
+          matchPackageNames: ["docker"],
+          matchDatasources: ["docker"],
+          matchManagers: ["custom.regex"],
+          customChangelogUrl: "https://github.com/moby/moby",
+        },
+      ],
+      customManagers: [
+        {
+          customType: "regex",
+          fileMatch: ["^.gitlab-ci.yml$"],
+          matchStrings: ['BASE_IMAGE: "(?<depName>.*):(?<currentValue>.*)"\\s'],
+          datasourceTemplate: "docker",
+        },
+        {
+          customType: "regex",
+          fileMatch: ["^.gitlab-ci.yml$"],
+          matchStrings: [
+            'DOCKER_DIND_IMAGE: "(?<depName>.*):(?<currentValue>.*)-dind"\\s',
+          ],
+          datasourceTemplate: "docker",
+        },
+        {
+          customType: "regex",
+          fileMatch: ["^opentofu_versions.yaml$"],
+          matchStrings: ["latest_version: '(?<currentValue>.*?)'\\n"],
+          depNameTemplate: "opentofu/opentofu",
+          datasourceTemplate: "github-releases",
+        },
+      ],
+      postUpgradeTasks: {
+        commands: ["./.gitlab/scripts/renovate-post-upgrade.sh"],
+        executionMode: "branch",
+      },
+    },
+  ],
   {
-    repository: "gitlab-renovate-forks/opentofu",
-    ...baseConfig,
-    extends: ["group:commitlintMonorepo"],
-    labels: [
-      ...defaultLabels,
-      "group::environments",
-      "Category:Infrastructure as Code",
-    ],
-    enabledManagers: ["gitlabci", "custom.regex"],
-    reviewers: ["timofurrer"],
-    packageRules: [
-      {
-        matchPackageNames: ["docker"],
-        matchDatasources: ["docker"],
-        matchManagers: ["custom.regex"],
-        customChangelogUrl: "https://github.com/moby/moby",
-      },
-    ],
-    customManagers: [
-      {
-        customType: "regex",
-        fileMatch: ["^.gitlab-ci.yml$"],
-        matchStrings: ['BASE_IMAGE: "(?<depName>.*):(?<currentValue>.*)"\\s'],
-        datasourceTemplate: "docker",
-      },
-      {
-        customType: "regex",
-        fileMatch: ["^.gitlab-ci.yml$"],
-        matchStrings: [
-          'DOCKER_DIND_IMAGE: "(?<depName>.*):(?<currentValue>.*)-dind"\\s',
-        ],
-        datasourceTemplate: "docker",
-      },
-    ],
-  },
-]);
+    allowedPostUpgradeCommands: ["^./.gitlab/scripts/renovate-post-upgrade.sh$"],
+  }
+);
