5047ee466af5c6ad44abd0067c4662c5bb207748
Alec Xu <axu@gitlab.com>
2024-08-07 14:30:37 -0700

    Move moby regexes to one entry

--- renovate/distribution/cng.config.js
+++ renovate/distribution/cng.config.js
@@ -52,17 +52,12 @@ module.exports = createServerConfig([
       },
       {
         customType: "regex",
-        fileMatch: [".gitlab-ci.yml"],
-        matchStrings: ['BUILDKIT_IMAGE: "(?<depName>.*):v(?<currentValue>.*)"\\s'],
-        depNameTemplate: "moby/buildkit",
-        datasourceTemplate: "docker",
-        extractVersionTemplate: "^v?(?<version>.*)$",
-      },
-      {
-        customType: "regex",
         includePaths: ["docs/*"],
-        fileMatch: ["docs/ci-variables.md"],
-        matchStrings: ['BUILDKIT_IMAGE[^`]+`(?<depName>[^:]+):v(?<currentValue>.*)`'],
+        fileMatch: [".gitlab-ci.yml", "docs/ci-variables.md"],
+        matchStrings: [
+          'BUILDKIT_IMAGE: "(?<depName>.*):v(?<currentValue>.*)"\\s',
+          'BUILDKIT_IMAGE[^`]+`(?<depName>[^:]+):v(?<currentValue>.*)`'
+        ],
         depNameTemplate: "moby/buildkit",
         datasourceTemplate: "docker",
         extractVersionTemplate: "^v?(?<version>.*)$",
